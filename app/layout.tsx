613c42d89ed5f399a254a2554974faa68ced454a
mr coding <davidhem1@outlook.fr>
2025-07-29 20:57:09 +0000

    my m3iza

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,9 @@ import { GeistMono } from 'geist/font/mono'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'my_m3iza',
+  description: 'Created By Mohamed',
+  generator: 'Hm dev',
 }
 
 export default function RootLayout({
